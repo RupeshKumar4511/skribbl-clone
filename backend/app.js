7c6a1dd88efb4c779ca16e85a082ab59ccaed2e9
RupeshKumar4511 <kumarrn4511@gmail.com>
2026-04-11 00:49:04 +0530

    app.js updated

--- backend/app.js
+++ backend/app.js
@@ -1,21 +1,42 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
+import http from 'node:http'
+import { Server } from 'socket.io'
 import cors from 'cors';
 
 const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: 'http://localhost:5173',
+        credentials: true
+    }
+})
 
 app.use(cors({
-    origin:'http://localhost:5173',
-    credentials:true
+    origin: 'http://localhost:5173',
+    credentials: true
 }));
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 
-// app.use('/api/v1/auth',authRoutes);
+io.on('connection',(socket)=>{
+    console.log("User Connected ")
+    console.log(socket.id);
+
+    socket.on('message',(data)=>{
+        console.log(data)
+    })   
+    
+    socket.on('disconnect',()=>{
+        console.log("user disconnected")
+    })
+})
 
 
-export default app;
+export default server;
